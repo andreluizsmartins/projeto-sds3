82440342b59b1375e151ece18b001e2d0147d31f
André Martins <andreluizsmartins@gmail.com>
2021-09-07 12:37:11 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/martins/dsvendas/entities/Seller.java
@@ -0,0 +1,42 @@
+package com.martins.dsvendas.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	
+	public Seller(Long id, String name, Sale sale) {
+		super();
+		this.id = id;
+		this.name = name;
+		
+	}
+	public Seller() {
+		
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
