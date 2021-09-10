0dd0a6e2dcb0a45276cbed542e54acf8a8cb2268
André Martins <andreluizsmartins@gmail.com>
2021-09-10 09:10:36 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,6 +1,27 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts'
-
+import { BASE_URL } from 'utils/request';
+type ChartDate = {
+    labels : string[];
+    series : number[];
+}
 const DonutChart = () => {
+    let chartData : ChartDate = {labels : [], series : []};
+
+    axios.get(`${BASE_URL}/sale/totalSales`)
+        .then(response => {
+            console.log(response);
+           
+            for (let key in response.data) {
+
+                chartData.labels.push(response.data[key][0]);
+                chartData.series.push(response.data[key][1]);
+
+            }
+            console.log(chartData);
+            }
+        )
+
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
@@ -13,8 +34,8 @@ const DonutChart = () => {
     }
     return (
         <Chart 
-            options={{...options, labels:mockData.labels}}
-            series={mockData.series}
+            options={{...options, labels:chartData.labels}}
+            series={chartData.series}
             type="donut"
             height="240"
         />
---
+++ frontend/src/types/sale.ts
@@ -0,0 +1,4 @@
+export type SaleSum = {
+    sellerName: string,
+    total: number
+}
